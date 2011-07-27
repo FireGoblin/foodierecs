5b34ad6fdc077dbed8b720187aa2896c7bb59a67
FireGoblin <FireGoblin21@gmail.com>
2011-07-27 19:50:10 -0400

    before opinion overhaul

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,11 @@ class PagesController < ApplicationController
 	
 	def main
 		@recommended = restaurantRecommendations(current_user)
-		@recommended = @recommended.paginate(:page => params[:page])
+		@recommended = @recommended.paginate(:page => params[:page], :per_page => 5)
 		@places_to_rate = placesToRate(current_user)
-		@places_to_rate = @places_to_rate.paginate(:page => params[:page])
+		@places_to_rate = @places_to_rate.paginate(:page => params[:page], :per_page => 5)
 		@recently_rated = recentlyRated
-		@recently_rated = @recently_rated.paginate(:page => params[:page])
+		@recently_rated = @recently_rated.paginate(:page => params[:page], :per_page => 5)
 	end
 	
 	def becomeafoodie
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '213050112074083', 'b661c121782409e1a06a8016cd775870', {:client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca-bundle.crt"}}}
+  provider :facebook, '213050112074083', 'b661c121782409e1a06a8016cd775870',  {:client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca-bundle.crt"}}}
   puts("#{Rails.root}")
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
