33fdb8b5bc632fd764e1a46fccf636ea6563d4e1
FireGoblin <FireGoblin21@gmail.com>
2011-07-27 20:51:04 -0400

    lots of changes

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -3,4 +3,8 @@ class Opinion < ActiveRecord::Base
 	belongs_to :restaurant
 	
 	default_scope :order => 'opinions.created_at DESC'
+	
+	def self.create_from_hash!(hash)
+  	create(:user_id => hash['user_id'], :restaurant_id => hash['restaurant_id'], :like => hash['like'], :foodie => hash['foodie'], :to_eat => "", :to_drink => "")
+	end
 end
