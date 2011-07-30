44f23c9ee8ee4dbb5252bc8a69ba7bd141c61fe9
Sidharth Kakkar <sdhrthk@gmail.com>
2011-07-29 23:51:52 -0400

    updating all the restaurants strings to 400 or more

--- db/migrate/20110720005754_create_restaurants.rb
+++ db/migrate/20110720005754_create_restaurants.rb
@@ -3,20 +3,20 @@ class CreateRestaurants < ActiveRecord::Migration
     create_table :restaurants do |t|
       t.string :name, :limit => 400
       t.string :address, :limit => 400
-      t.string :street_number
-      t.string :street
-      t.string :phone
+      t.string :street_number, :limit => 400
+      t.string :street, :limit => 400
+      t.string :phone, :limit => 400
       t.float :lat
       t.float :long
-      t.string :icon
-      t.string :google_id
+      t.string :icon, :limit => 400
+      t.string :google_id, :limit => 400
       t.string :reference, :limit => 500
-      t.string :type1
-      t.string :type2
-      t.string :type3
-      t.string :type4
-      t.string :url
-      t.string :vicinity
+      t.string :type1, :limit => 400
+      t.string :type2, :limit => 400
+      t.string :type3, :limit => 400
+      t.string :type4, :limit => 400
+      t.string :url, :limit => 400
+      t.string :vicinity, :limit => 400
       t.float :rating
       t.integer :foodie_likes
       t.integer :foodie_dislikes
