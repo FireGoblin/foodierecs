0eddb7834e77dc0514d2868ce0e55fd008c6d94a
Sidharth Kakkar <sdhrthk@gmail.com>
2011-08-05 12:15:07 -0400

    making the background load last and cycle through

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,15 +10,17 @@ class PagesController < ApplicationController
 	def main
 	  if( current_user.nil? )
 	    #WHY DOESN'T THIS WORK?????
-	    redirect_to "auth/facebook"
-    end
+	    puts "\n\n IT'S NIL \n\n"
+	    redirect_to "/auth/facebook"
+    else
     
-		@recommended = restaurantRecommendations(current_user)
-		@recommended = @recommended.paginate(:page => params[:page], :per_page => 5)
-		@places_to_rate = placesToRate(current_user)
-		@places_to_rate = @places_to_rate.paginate(:page => params[:page], :per_page => 9)
-		@recently_rated = recentlyRated
-		@recently_rated = @recently_rated.paginate(:page => params[:page], :per_page => 5)
+	  	@recommended = restaurantRecommendations(current_user)
+  		@recommended = @recommended.paginate(:page => params[:page], :per_page => 5)
+  		@places_to_rate = placesToRate(current_user)
+  		@places_to_rate = @places_to_rate.paginate(:page => params[:page], :per_page => 9)
+  		@recently_rated = recentlyRated
+  		@recently_rated = @recently_rated.paginate(:page => params[:page], :per_page => 5)
+	  end
 	end
 	
 	def becomeafoodie
